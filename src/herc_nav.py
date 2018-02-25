9c4ee28bc8f0565a5241e4c5268a05533248a401
jreyes81 <jreyes81@calstatela.edu>
2018-02-24 19:07:34 -0800

    Updated navigation code to inlucde forward movement and slight forward movement

--- src/herc_nav.py
+++ src/herc_nav.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python
-# Hercules Navigation Code
+# Hercules Navigation Code : Takes in information from ultrasounds and process the information to
+#                            determine the path the robot will navigate through.
 #
 # Created By: Jeovanny Reyes
 # Created On: February 24, 2018
 #
 # Subscriber: Subscribes to message topic "/HerculesUltrasound_Range" containing "Range" data
-# Publisher: Publishes to message topic "cmd_vel" with "geometry_msgs/Twist" data
+# Publisher:  Publishes to message topic "cmd_vel" with "geometry_msgs/Twist" data
 #
 # Raytheon Radar Guided Rescue robot
 # Cal State LA
 
 import math
 import sys
+import rospy
+
+from sensor_msgs.msg import Range # Ultrasound
+from geometry_msgs.msg import Twist # Robot movements
+
+class Navigation(object):
+    def __init__(self):
+        rospy.init_node("navigation_node", anonymous=True):
+
+        # Initializing values
+        self.real_obj_dist = 0
+        self.twist = Twist()
+        self.twist.linear.x = 0
+        self.twist.angular.z = 0
+
+        # Subscriber
+        self.ran_sub = rospy.Subscriber("HerculesUltrasound_Range", Range, self.distcallback) #Used to be Float32. [Float 32, callback])
+        # Publisher
+        self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=100) # Rate set in Hz
+
+        rate = rospy.Rate(10) # Pubslishing at 10 hz
+
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+    def distcallback(self,range): # Takes in message "range" as input. Data comes back in cm
+        real_obj_dist = range.range
+        self.real_obj_dist = real_obj_dist
+
+    def move(self): # Function to make robot move
+        self.real_obj_dist = math.fabs(self.real_obj_dist) # Returns the absolute value of distance
+
+        if ((self.real_obj_dist < 100) && (self.real_obj_dist > 0)):
+            self.twist.linear.x = 20 # Robot moves forward at 20% speed
+            self.twist.angular.z = 0 # Robot does not rotate
+            self.cmd_pub.publish(self.twist) # Publishes
+
+        if self.real_obj_dist > 100:
+            self.twist.linear.x = 0
+            self.twist.angular.z = 20
+            self.cmd_pub.publish(self.twist)
+
+
+if __name__ == '__main__'
+    try:
+        Navigation()
+    except rospy.ROSInterruptionException:
+        pass
