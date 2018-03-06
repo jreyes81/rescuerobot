819d25618d7d89b823deaa2eab678889097cada0
jreyes81 <jreyes81@calstatela.edu>
2018-03-05 17:18:08 -0800

    Created module to make robot stop

---
+++ src/robot_stop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Robot Stop code: Imports information from radar display (When "STOP" button is pressed). Information comes back as
+#		   "True" or "False"  
+#
+# Created By: Jeovanny Reyes
+# Created On: March 3, 2018
+#
+#
+# Raytheon Radar Guided Rescue robot
+# Cal State LA
+
+import sys
+import rospy
+
+from pygame_radar import RadarDisplay
+
+def Stop_Info():	
+	
+
