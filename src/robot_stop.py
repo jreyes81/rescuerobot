ee50df4100075755ae85aa3ac571c678ddcc80a8
jreyes81 <jreyes81@calstatela.edu>
2018-03-08 18:18:28 -0800

    Added function inside module

--- src/robot_stop.py
+++ src/robot_stop.py
@@ -13,27 +13,29 @@ import time
 import pygame
 
 from text_object_black import text_object_black
+STOP_BUTTON_X_SCALE = 0.030
+is_mouse_over_button_bool = False
 
-def stop_button(xpos,ypos,stop_val,brightred,red):
+def stop_button(xpos,ypos,stop_val,brightred,red): 
 	mouse = pygame.mouse.get_pos()	 # Obtains position of mouse
-	click = pygame.mouse.get_pressed() # Obtains information when mouse gets clicked
-	fontsize = int(xpos * 0.030)
+	click = pygame.mouse.get_pressed()[0] # Obtains information when mouse gets clicked
+	fontsize = int(xpos * STOP_BUTTON_X_SCALE)
 	smalltext = pygame.font.Font("freesansbold.ttf",30)
 	x_pos = xpos * 0.050 # X Coordinate for button position
 	y_pos = ypos * 0.035 # Y Coordinate for button position
 	x_length = xpos * 0.150 # Length of "STOP" button
 	y_width = ypos * 0.050 # Width of "STOP" button
 
-	stop = stop_val # Initialy contains False boolean
+	stop = stop_val # Initialy contains "False" boolean
 
 	xposlength = x_pos + x_length
 	yposwidth = y_pos + y_width
 
 	# return xposlength, yposwidth, mouse, x_pos, y_pos, x_length, y_width
-
-	if ((xposlength > mouse[0] > x_pos) and (yposwidth > mouse[1] > y_width)): # Hovering over box
+	is_mouse_over_button_bool = is_mouse_over_button(xposlength, x_pos, yposwidth, y_width)
+	if is_mouse_over_button: # Hovering over box
 		pygame.draw.rect(pygame.display.get_surface(),brightred,(x_pos,y_pos,x_length,y_width))
-		if click [0] == 1: # When we left click on the "STOP" button
+		if click: # When we left click on the "STOP" button
 			print('Robot has stopped moving!')
 			stop = True # Value is then set to Stop
 	else:
@@ -42,5 +44,10 @@ def stop_button(xpos,ypos,stop_val,brightred,red):
 	textSurf,textRect = text_object_black("STOP",smalltext)
 	textRect.center = ((x_pos +(x_length/2)), y_pos+(y_width/2))
 	pygame.display.get_surface().blit(textSurf, textRect)
+	return stop
+#def tranf_stop
 
-def tranf_stop
+def is_mouse_over_button(xposlength, x_pos, yposwidth, y_width):
+	"""Detects if mouse is hovering over a button"""
+	mouse = pygame.mouse.get_pos()	
+	return ((xposlength > mouse[0] > x_pos) and (yposwidth > mouse[1] > y_width))
