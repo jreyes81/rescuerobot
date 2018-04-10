cf2cf75e35af1ea1443947803c9f1dfd958154ad
jreyes81 <jreyes81@calstatela.edu>
2018-04-09 23:22:40 -0700

    Working arduino code for motor control. Tested with turning left, then right, then goes straight

--- arduino/hbridge_subscriber_ver2/hbridge_subscriber_ver2.ino
+++ arduino/hbridge_subscriber_ver2/hbridge_subscriber_ver2.ino
@@ -45,85 +45,91 @@ ros::NodeHandle nh; // instantiating node handle to create publishers and subscr
 
 std_msgs::Float32 int_msg; // Creating message instance
 
-int ENA = 30;//teensy pin 30. For Right motor
-int ENB = 2; //teensy pin 2. For left motor
+int ENA = 30;//teensy pin 30. For Left Motor
+int ENB = 2; //teensy pin 2. For Right Motor
 
 //right motors
-int IN1 = 10;
+//int IN1 = 10;
 int IN2 = 9; // Only using this one!!
 
 //left motors
-int IN3 = 7;
+//int IN3 = 7;
 int IN4 = 6; // Only using this one!!
 
 float linear = 0; // Contains linear velocity commands
 float angular = 0; // Contains angular velocity commands
+float to_stop = 0;
 
-  
-void wheel_cb( const geometry_msgs::Twist& cmd_msg){ // message name cmd_msg
-  linear = cmd_msg.linear.x;
-  angular = cmd_msg.angular.z;
-  //int_msg.data = 1; //Sends info about scan mode to have robot not scan!
-}
 
 void straight() // Function to make robot go straight
 {
-    analogWrite(ENA, 250); // This sets the speed of the motor
-    analogWrite(ENB, 250); // This sets the speed of the motor
-    digitalWrite(IN1, LOW);
+    analogWrite(ENA, 150); // This sets the speed of the left motor
+    analogWrite(ENB, 150); // This sets the speed of the right motor
+    //digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
+    //digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);  // Might need to put a delay to make robot go forwards for a few seconds
     //int_msg.data = 2 //send value of 2 to have robot scan again in "Data Collection Mode"
   }
 
 void right_turn() // Function to make robot go right
 {
-    analogWrite(ENB, 250); // This sets the speed of the left motor
-    digitalWrite(IN3, HIGH);
+    analogWrite(ENA, 150); // For left motor
+    //delay(2000); Does not work here!!
+    analogWrite(ENB, -150);
+    //delay(2000); Does not work here !!
     digitalWrite(IN4, HIGH);
+    //delay(2000); // Waits 2 seconds before left wheel starts moving!
+    digitalWrite(IN2, HIGH);
+    
   }
 
 void left_turn() // Function to make robot go left
 {
-  analogWrite(ENA, 250); //This sets thje speed of the right motor
-  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, -150);
+  analogWrite(ENB, 150);
+  //digitalWrite(ENB, HIGH); // For left motor
+  //analogWrite(ENB, 200); // This sets the speed of the left motor
+  //digitalWrite(ENA, LOW); 
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN4, HIGH);
   }
 
-int stop_robot() // Function to make robot stop
+void stop_robot() // Function to make robot stop
 {
-  digitalWrite(ENA, LOW); // right motor stops
-  digitalWrite(ENB, LOW); // left motor stops
+  analogWrite(ENA, 5);
+  analogWrite(ENB, 5);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN4, HIGH);
   }
 
- 
-
-ros::Subscriber<geometry_msgs::Twist> sub_cmd_vel("cmd_vel" , wheel_cb); 
 ros::Publisher scan_once_return("scan_once_return", &int_msg);
 
-void setup()
-{
-  nh.initNode();
-  nh.advertise(scan_once_return); // Publishing to topic "scan_once_mode"
-  nh.subscribe(sub_cmd_vel); // Subscribing to topic "cmd_vel" with nickname sub_cmd_vel
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+void wheel_cb( const geometry_msgs::Twist& cmd_msg){ // message name cmd_msg. Used to be const instead of int
+  linear = cmd_msg.linear.x;
+  angular = cmd_msg.angular.z;
+  to_stop = cmd_msg.linear.z;
   //int_msg.data = 1; //Sends info about scan mode to have robot not scan!
-}
 
-void loop()
-{
-  //scan_once_return.publish( &int_msg );
-  nh.spinOnce(); // callback gets implemented
+  if (linear == 20){
+    left_turn();
+    delay(2000);
+    if (angular == 20){
+      right_turn();
+      delay(2000);
+      if (to_stop == -1){
+        straight();
+        delay(2000);
+      }
+    }
+  }
+
   if (linear == 20 && angular == 0)// Makes robot go forward
   {
     right_turn();
-    straight(); //robot goes straight
-    delay(2000); // For 2 seconds
+    //delay(20);
+    //straight(); //robot goes straight
+    //delay(20); // For 2 seconds
     stop_robot(); // Robot then stops
     int_msg.data = 2; // State data to make robot scan again
     scan_once_return.publish( &int_msg ); // state info of 2 (robot scan again)
@@ -132,6 +138,7 @@ void loop()
 
    if (linear == 0 && angular == 20)// Makes robot rotate right
    {
+    straight();
     right_turn(); // robot rotates right
     delay(1000); // For 1 second
     straight(); // robot goes straight
@@ -155,6 +162,30 @@ void loop()
       stop_robot(); // Function to make robot stop
       int_msg.data = 2; // State data to make robot scan again
     }
+}
+ 
+
+ros::Subscriber<geometry_msgs::Twist> sub_cmd_vel("cmd_vel" , wheel_cb); 
+//ros::Publisher scan_once_return("scan_once_return", &int_msg);
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(scan_once_return); // Publishing to topic "scan_once_mode"
+  nh.subscribe(sub_cmd_vel); // Subscribing to topic "cmd_vel" with nickname sub_cmd_vel
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  //pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  //pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  //int_msg.data = 1; //Sends info about scan mode to have robot not scan!
+}
+
+void loop()
+{
+  //scan_once_return.publish( &int_msg );
+  nh.spinOnce(); // callback gets implemented
   //scan_once_return.publish( &int_msg ); // state info of 2 (robot scan again)
   //delay(1);
 }
