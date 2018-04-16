f0bd3ebb2549aebc2fed433044de6a71086360d7
jreyes81 <jreyes81@calstatela.edu>
2018-04-16 16:53:08 -0700

    changed robot_state function

--- src/herc_nav.py
+++ src/herc_nav.py
@@ -41,17 +41,17 @@ class Navigation(object):
         self.go_to_sect5 = False # Used to tell if robot will navigate through sector 5
         self.go_to_sect6 = False # Used to tell if robot will navigate through sector 6
         self.state_of_radar = True
-        self.real_obj_dist_array = [0] *4101 # Will store data from ultrasound. Ued to be 4096. We will process this data
+        self.real_obj_dist_array = [0] *460 # Will store data from ultrasound. Takes about 459-465 points worth of raw data for one complete sweep. Used to be 4101
         self.fake_obj_dist = 0 # Used to store data when we re done sweeping. It is a holder so we don't process it.
-        self.sect1 = [0]*454 # Each sector is incremented by 20 degrees. 227 data points from first half sweep
-        self.sect2 = [0]*454
-        self.sect3 = [0]*454
-        self.sect4 = [0]*454
-        self.sect5 = [0]*454
-        self.sect6 = [0]*454
-        self.sect7 = [0]*454
-        self.sect8 = [0]*454
-        self.sect9 = [0]*454
+        self.sect1 = [0]*52 # Each sector is incremented by 20 degrees. 227 data points from first half sweep
+        self.sect2 = [0]*51
+        self.sect3 = [0]*51
+        self.sect4 = [0]*51
+        self.sect5 = [0]*51
+        self.sect6 = [0]*51
+        self.sect7 = [0]*51
+        self.sect8 = [0]*51
+        self.sect9 = [0]*51 # Will contain an additional point of data
 
 
         # Subscriber
@@ -84,7 +84,7 @@ class Navigation(object):
                 self.scan_once_return = 1
                 self.pub_scan_once_return() # Returns a value of 1 to have radar know we received a 0 and we won't move
 
-            while self.scan_once_state == 1 or self.scan_once_state == 2: # Robot dones scanning and ready to process data
+            while self.scan_once_state == 1: #or self.scan_once_state == 2: # Robot dones scanning and ready to process data
                 self.done_processing_data = False
                 #self.scan_once_return = 2
                 #self.pub_scan_once_return() # Returns a value of 2 to let radar node we know it is done sweeping and we are going to move
@@ -121,39 +121,49 @@ class Navigation(object):
         self.stop = data.data
 
     def store_data_in_sectors(self):
+        # 459 points is the sweep from 5 to 175 degrees and back
+        # 230 points is from 5 to 175 degrees!
+        # 229 points is from 175 degrees to 5!
+        # The index value at 230 of the array will be zero and willbe stores in sector 9 since we won't process this
         # Storing data from 0 to 180 degree sweep!
-        for i in range(227):
-            self.sect1[i] = self.real_obj_dist_array[i] # From 0 to 226
-            self.sect2[i] = self.real_obj_dist_array[227+i] # From 227 to 454
-            self.sect3[i] = self.real_obj_dist_array[455+i] # From 455 to 682
-            self.sect4[i] = self.real_obj_dist_array[683+i] # From 683 to 910
-            self.sect5[i] = self.real_obj_dist_array[911+i] # From 911 to 1138
-            self.sect6[i] = self.real_obj_dist_array[1139+i] # From 1139 to 1366
-            self.sect7[i] = self.real_obj_dist_array[1367+i] # From 1367 to 1594
-            self.sect8[i] = self.real_obj_dist_array[1595+i] # From 1595 to 1822
-            self.sect9[i] = self.real_obj_dist_array[1823+i] # From 1823 to 2050
+        for i in range(26): # Used to be 227.
+        # Sectors 3,4,5,6 and 7 will receive 26 data points. Sectors 1,2,8 and 9 will receive 25
+            self.sect3[i] = self.real_obj_dist_array[50+i] # From 50 to 75
+            self.sect4[i] = self.real_obj_dist_array[76+i] # From 76 to 101
+            self.sect5[i] = self.real_obj_dist_array[102+i] # From 102 to 127
+            self.sect6[i] = self.real_obj_dist_array[128+i] # From 128 to 153
+            self.sect7[i] = self.real_obj_dist_array[154+i] # From 154 to 179
+
+        for i in range(25):
+            self.sect1[i] = self.real_obj_dist_array[i] # From 0 to 24
+            self.sect2[i] = self.real_obj_dist_array[25+i] # From 25 to 49
+            self.sect8[i] = self.real_obj_dist_array[180+i] # From 180 to 204
+            self.sect9[i] = self.real_obj_dist_array[205+i] # From 205 to 229
 
         # Storing data from 180 to 0 degree sweep!
-        for j in range(227):
-            self.sect1[j] = self.real_obj_dist_array[3874+i] # From 3874 to 4101
-            self.sect2[j] = self.real_obj_dist_array[3646+i] # From 3646 to 3873
-            self.sect3[j] = self.real_obj_dist_array[3418+i] # From 3418 to 3645
-            self.sect4[j] = self.real_obj_dist_array[3190+i] # From 3190 to 3417
-            self.sect5[j] = self.real_obj_dist_array[2962+i] # From 2962 to 3189
-            self.sect6[j] = self.real_obj_dist_array[2734+i] # From 2734 to 2961
-            self.sect7[j] = self.real_obj_dist_array[2506+i] # From 2506 to 2733
-            self.sect8[j] = self.real_obj_dist_array[2278+i] # From 2278 to 2505
-            self.sect9[j] = self.real_obj_dist_array[2050+j] # From 2050 to 2277
+        for j in range(25): # Used to be 227
+        # Sectors 3,4,5,6 and 7 will have 26 data points. Sectors 1,2,8, and 9 will receive 25
+            self.sect9[j] = self.real_obj_dist_array[230+j] # From 230 to 254
+            self.sect8[j] = self.real_obj_dist_array[255+j] # From 255 to 279
+            self.sect2[j] = self.real_obj_dist_array[410+j] # From 410 to 434
+            self.sect1[j] = self.real_obj_dist_array[435+j] # From 435 to 459
+
+        for j in range(26):
+            self.sect7[j] = self.real_obj_dist_array[280+j] # From 280 to 305
+            self.sect6[j] = self.real_obj_dist_array[306+j] # From 306 to 331
+            self.sect5[j] = self.real_obj_dist_array[332+j] # From 332 to 359
+            self.sect4[j] = self.real_obj_dist_array[360+j] # From 360 to 383
+            self.sect3[j] = self.real_obj_dist_array[384+j] # From 384 to 409
 
     def process_data(self):
         # We process and analyze the information stored in sectors 4,5 and 6
         # Based on stored distance information, we set information about the sectors and ir robot can navigate through them
-        for i in range(454):
-            if self.sect5[i] > 58: # Checking to see if there is an object in sector 5 within 60 cm
+        for i in range(26): # Used to be 51
+            if abs(self.sect5[i]) >= 0: # Checking to see if there is an object in sector 5 within 60 cm
                 self.go_to_sect5 = True # There is no object detected within the 60 cm threshold of sector 5
-            if self.sect4[i] > 58: # Checking to see if there is an object in sector 4 within 60 cm
+            if abs(self.sect4[i]) >= 28: # Checking to see if there is an object in sector 4 within 60 cm
                 self.go_to_sect4 = True # There is no object detected within the 60 cm threshold of sector 4
-            if self.sect6[i] > 58: # There is no object detected within the 60 cm threshold of sector 6
+            if abs(self.sect6[i]) >= 28: # There is no object detected within the 60 cm threshold of sector 6
                 self.go_to_sect6 = True # There is no object detected within the 60 cm threshold of sector 5
         self.robot_on_standby= False
         self.done_processing_data = True # Data has been processed, and robot is now ready to move!
@@ -162,7 +172,7 @@ class Navigation(object):
     def move(self): # Function to make robot move
 
             if self.robot_on_standby == False: # Robot has finished collecting data and is processing it
-                if self.done_processing_data == True: # Robot has finished processing data and ready to move!
+                #if self.done_processing_data == True: # Robot has finished processing data and ready to move!
                     # self.scan_once_return = 1 # We will publish this value to have radar sweep again after we are done moving
 
                     # Op Mode: Stop : When Emergency stop button is pressed
@@ -185,13 +195,13 @@ class Navigation(object):
                         if self.count >= 150:
                             self.twist.linear.x = 0 # Robot stops
                             self.twist.angular.z = 0
-                            self.scan_once_return = 3
+                            self.scan_once_return = 1
                             self.cmd_pub.publish(self.twist)
                             #self.count = 0 # Count variable reset so it can be reused
                             self.pub_scan_once_return() # Returns a value of 2 to have radar sweep again
 
                     # Op Mode 2: Navigate Right Side Around Object : Object is within 60 cm and stop button hasn't been pressed
-                    if self.stop == False and self.go_to_sect5 == False and self.go_to_sect4 == False:
+                    if self.stop == False and self.go_to_sect6 == True:#self.go_to_sect5 == False and self.go_to_sect6 == True:
                         print('Hercules Going Right!')
                         if self.count < 150: # It takes about 5 seconds to get to a 300 count!
                             self.twist.linear.x = 0 # Robot steers right first
@@ -214,13 +224,13 @@ class Navigation(object):
                         if self.count >= 450:
                             self.twist.linear.x = 0
                             self.twist.angular.z = 0 # Robot then stops after a few seconds
-                            self.scan_once_return = 3 # We will publish this value to have radar sweep again after we are done moving
+                            self.scan_once_return = 1 # We will publish this value to have radar sweep again after we are done moving
                             self.cmd_pub.publish(self.twist)
                             #self.count = 0 # Reseting count variable to be reused
                             self.pub_scan_once_return() # Returns a value of 1 to have radar sweep again
 
                     # Op Mode 3: Navigate Left Side Around Object : Object is within 60 cm and stop button hasn't been pressed
-                    if self.stop == False and self.go_to_sect5 == False and self.go_to_sect4 == True:
+                    if self.stop == False and self.go_to_sect4 == True:#self.go_to_sect5 == False and self.go_to_sect4 == True:
                         print('Hercules Going Left!')
                         if self.count < 150: # About 2.5 second travel time
                             self.twist.linear.x = 0
