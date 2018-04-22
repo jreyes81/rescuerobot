122e9b5cacad818dc813e6a8b932c09ec3f67abb
jreyes81 <jreyes81@calstatela.edu>
2018-04-21 18:00:14 -0700

    Final working code

--- src/pygame_radar_ver1.py
+++ src/pygame_radar_ver1.py
@@ -74,10 +74,6 @@ class RadarDisplay():
 
     def scancb(self,data):
         self.scan_once_from_herc = data.data # data comes out as 1 or 2
-        #if self.scan_once_from_herc == 1: # Right when we recieve a 3 telling the radar to sweep again. A 3 comes when robot is done moving
-        #    self.scan_once_to_herc = 0 # A 0 gets publish to the nav code telling the robot not to move as it is sweeping and collecting
-        #if self.scan_once_from_herc == 3:
-        #    self.scan_once_to_herc = 0
 
     def distcallback(self,range): # Takes in message "range" as input. Data comes back in cm
         real_obj_dist = range.range
@@ -108,17 +104,16 @@ class RadarDisplay():
 
         Rrx2 = [0] *512
         Rry2 = [0] *512
-        #self.pub_scan_once_state() # self.scan_once_to_herc Publishes 0 initially
 
         while not rospy.is_shutdown():
           self.rate.sleep()
           self.pub_scan_once_state()
-          print("made it inside th while not rospy loop")
-        #   self.pub_scan_once_state() # self.scan_once_to_herc Publishes 0 initially
-          #if self.scan_once_from_herc == 1:
-            #  self.scan_once_to_herc = 0
+          print("made it inside the while not rospy loop")
 
-          while (self.scan_once_to_herc == 0 or self.scan_once_from_herc == 1): # Radar is scanning and robot is NOT moving
+          Rrx = [0]*512
+          Rry = [0] *512
+
+          while (self.scan_once_to_herc == 0 ):#or self.scan_once_from_herc == 1): # Radar is scanning and robot is NOT moving
                 #self.count_for_array_size = self.count_for_array_size + 1
                 for i in range(1024): # 4096 (Used to be this value) and then 2048, now 1024. covers entire circle and is how many times radius line is drawn
                    self.pub_stop_button_state() # Publishes state information about stop button. Continous "False" when it is not pressed and continous "True" when presses
@@ -133,10 +128,7 @@ class RadarDisplay():
                        self.count = 0
                        self.scan_once_to_herc = 1 # To make radar display stop sweeping
                        self.Transition = 1
-                       #self.pub_scan_once_state()
-                       #self.scan_once_from_herc = 2
                        break # Break from the loop that is ploting
-                       #self.scan_once_display = False
                    self.count = self.count + 1
 
                    if i%6==0: # Used to be 8 pygame draws, now its 6print. Only goes into here hen i=0,8,16, etc.
@@ -238,14 +230,17 @@ class RadarDisplay():
                 if self.Transition == 1:
                     self.scan_once_to_herc = 1
                     break
+        #   pygame.time.delay(1500)
           self.scan_once_to_herc = 1
           self.pub_scan_once_state()
-          while (self.scan_once_to_herc == 1 or self.scan_once_from_herc == 2): # Radar display is not scanning and robot is moving!
+
+          while (self.scan_once_to_herc == 1):# or self.scan_once_from_herc == 2): # Radar display is not scanning and robot is moving!
                 print("I am not plotting")
+                radar_display_angle = 0
                 for i in range(950):
                     self.pub_stop_button_state() # Publishes state information about stop button. Continous "False" when it is not pressed and continous "True" when presses
                     self.pub_scan_once_state() # Publishes 0 continous when radar is still scanning, Publishes 1 continous when its done
-                    if self.count == 950:
+                    if self.count == 475:
                         self.count = 0
                         self.Transition = 2
                         self.scan_once_to_herc = 0
@@ -264,7 +259,8 @@ class RadarDisplay():
                     self.stop = stop_button(self.sx,self.sy,self.stop,self.brightred,self.red) # Stop Button Feature
                     labels(self.sx,self.sy,self.real_obj_dist)
                     arc_inc(self.sx,self.sy)
-                    range_to_string(self.sx,self.sy,self.real_obj_dist,self.servo_pos)
+                    # range_to_string(self.sx,self.sy,self.real_obj_dist,self.servo_pos)
+                    range_to_string(self.sx,self.sy,self.real_obj_dist,radar_display_angle)
                     linesections(self.sx,self.sy,self.green)
 
                     pygame.display.update()
@@ -276,124 +272,14 @@ class RadarDisplay():
                             pygame.quit()
                             exit()
 
-                if self. Transition == 2:
+                if self.Transition == 2:
                     self.scan_once_to_herc = 0
                     self.Transition = 0
                     break
           self.scan_once_to_herc = 0
           self.pub_scan_once_state()
-        #   if (self.scan_once_to_herc == 2 and self.scan_once_from_herc==3): # Radar is scanning and robot is NOT moving
-        #       print("I should be plotting")
-        #       for i in range(950): # 4096 (Used to be this value) and then 2048, now 1024. covers entire circle and is how many times radius line is drawn
-        #         self.pub_stop_button_state() # Publishes state information about stop button. Continous "False" when it is not pressed and continous "True" when presses
-        #         self.pub_scan_once_state() # Publishes 2 continous when radar is still scanning, Publishes 1 continous when its done
-        #         #self.angle = i * (2 * math.pi)/72 #  Radar sweep line is incremented by 0.08 degrees (6.283 is 2 pi). Originally divided by 72
-        #         self.angle = i * (360)/950 # This is in degrees: Used to be 360/2048 = .175, then it was 360/1024=.3516. Now its 360/1000=.36
-        #         # If we divided by 52 it takes 8.6 seconds to sweep from 0 to 180 degrees. 42 takes 6.7. 32 takes 5.2. 22 takes 3.43
-        #         if self.count2 == 950: # Use to be 4096, then 2048, now
-        #             self.count2 = 0
-        #             #self.scan_once_to_herc = 1 # To make radar display stop sweeping
-        #             break # Break from the loop that is ploting
-        #             #self.scan_once_to_herc = 1
-        #             #self.scan_once_display = False
-        #         self.count2 = self.count2 + 1
-          #
-        #         if i%8==0: # Used to be 8 pygame draws, now its 6print
-        #            pygame.draw.circle(screen, self.green, (self.sx/self.pos, self.sy/self.pos), (self.sx/2) - (self.sx/10), 1) # Outer circle
-        #            pygame.draw.circle(screen, self.green, (self.sx/self.pos, self.sy/self.pos), (self.sx/10), 1) #Inner circle. radius was sx/pos/5*1
-        #            pygame.draw.circle(screen, self.green, (self.sx/self.pos, self.sy/self.pos), (self.sx/5), 1) # 2nd Inner circle. radius was sx/pos/5*2
-        #            pygame.draw.circle(screen, self.green, (self.sx/self.pos, self.sy/self.pos), (self.sx/5) + (self.sx/10), 1) # 3rd Inner Circle. radius was sx/pos/5*3
-        #            pygame.draw.line(screen, self.green, ((self.sx/10), self.sy/self.pos), ((self.sx - (self.sx/10)), self.sy/self.pos)) # Horizontal Line. Origianlly from (0, sy/pos) to (sx,sy/pos)
-        #            #pygame.draw.line(screen, self.brightred, (sx/self.pos, 100), (sx/self.pos, 900)) # Vertical Line
-        #            pygame.draw.line(screen, self.brightblue, (self.sx/self.pos, (self.sy/10)), (self.sx/self.pos, (self.sy/2))) # Vertical Line
-        #            #print ("Landed in i statement")
-        #            # Modules imported and plotted
-        #            self.stop = stop_button(self.sx,self.sy,self.stop,self.brightred,self.red) # Stop Button Feature
-        #            labels(self.sx,self.sy,self.real_obj_dist)
-        #            arc_inc(self.sx,self.sy)
-        #            range_to_string(self.sx,self.sy,self.real_obj_dist,self.servo_pos)
-        #            linesections(self.sx,self.sy,self.green)
-          #
-        #            if 0 <= self.count2 <= 475: #Used to be up to 1024, now 900
-        #               #dx = self.sx/2 - self.sx/2 * math.cos(math.radians(self.angle)) # Starts from left side
-        #               for j in range(512): # 512 is the number of radar points drawn for entire circle. Used to be 512, then 256, now 512 again
-        #                   #deg = j * 5.625 / 8 # Increments by 0.703125 degrees. Used to be 5.625/8, then it was 4.21875/6, try 2.8125/4
-        #                   deg =  j * 5.625/8
-        #                   radar_deg = deg - self.angle # For first iteration we have 0.7 - 0.08 = 0.62 degrees. at j=206 we get 144.84 - 180
-        #                   if radar_deg <=0 :
-        #                      col = int(255*((360+radar_deg)/360)**1.3) # col is color. Color. Used to be **1.3. this one darkens
-        #                      pygame.draw.circle(screen, (0,col,0),(Rrx2[j-1],Rry2[j-1]),5) # Plots previous point data
-        #                   else:
-        #                      col = int(255*(radar_deg/360)**1.6) # Color is light
-        #                      pygame.draw.circle(screen, (0,col,0),(Rrx2[j-1],Rry2[j-1]),5) # Plots previous point data
-          #
-        #               if self.estimated_obj_dist < 0: # If object distance is smaller than 100 cm
-        #                   self.estimated_obj_dist = math.fabs(self.estimated_obj_dist) # Returns the absolute value of distance
-        #                   pygame.display.get_surface().blit(textSurfstop, textRectstop)
-        #               elif self.estimated_obj_dist > 8: # If object distance is greater than 100 cm. It gets plotted at origin. 8 is the radius of the big circle
-        #                   self.estimated_obj_dist = 0
-        #               x = self.sx/2 - (self.sx/2.5)* math.cos(math.radians(self.angle))
-        #               #dy = self.sy/2 - self.sx/2 * math.sin(math.radians(self.angle)) # Starts from top side
-        #               y = self.sy/2 - (self.sy/2.5) * math.sin(math.radians(self.angle))
-        #               # anti aliasing line: To make line smooth
-        #               pygame.draw.aaline(screen, self.brightred, (self.sx/2, self.sy/2), (x, y),5) # Takes about 10 seconds to sweep 180 degrees. Used to be dx and dy
-          #
-        #               rx = int(self.sx/2 - 50 * self.estimated_obj_dist * math.cos(math.radians(self.angle))) # Starts plotting left
-        #               ry = int(self.sy/2 - 50 * self.estimated_obj_dist * math.sin(math.radians(self.angle))) # Starts Plotting on top
-        #               Rrx2[i/8] = rx
-        #               Rry2[i/8] = ry
-        #               pygame.display.update()
-        #               pygame.time.wait(30) # Sleeps the gui for 30 milliseonds to share CPU. Share with ROS
-        #               # Could also use pygame.time.delay() instead of time.wait
-        #               screen.fill((0, 20, 0, 0))
-          #
-        #            elif 475 < self.count2 <= 950: #Used to be from 1024 to 2048
-        #               #dx = self.sx/2 - self.sx/2 * math.cos(math.radians(self.angle)) # Starts from right side
-        #               for j in range(512): # 512 is the number of radar points drawn for entire circle. Used to be 512, then 256, now 512 again
-        #                   #deg = j * 5.625 / 8 # Increments by 0.703125 degrees. Used to be 5.625/8, then it was 4.21875/6, try 2.8125/4
-        #                   deg =  j * 5.625/8
-        #                   radar_deg = deg - self.angle # For first iteration we have 0.7 - 0.08 = 0.62 degrees. at j=206 we get 144.84 - 180
-        #                   if radar_deg <=0 :
-        #                      col = int(255*(abs(radar_deg)/360)**1.6) # This one brightens
-        #                      pygame.draw.circle(screen, (0,col,0),(Rrx2[j-1],Rry2[j-1]),5) # Plots previous point data
-        #                    #   col = int(255*((360-radar_deg)/360)**1.6) # col is color. Color. Used to be **1.3. this one darkens
-        #                    #   pygame.draw.circle(screen, (0,col,0),(Rrx[j-1],Rry[j-1]),5) # Plots previous point data
-        #                   else:
-        #                      col = int(255*((abs(radar_deg))/360)**1.6) # col is color. Color. Used to be **1.3. this one darkens
-        #                      pygame.draw.circle(screen, (0,col,0),(Rrx2[j-1],Rry2[j-1]),5) # Plots previous point data
-        #                    #   col = int(255*(abs(radar_deg)/360)**1.6) # This one brightens
-        #                    #   pygame.draw.circle(screen, (0,col,0),(Rrx[j-1],Rry[j-1]),5) # Plots previous point data
-          #
-        #               if self.estimated_obj_dist < 0: # If object distance is smaller than 100 cm
-        #                   self.estimated_obj_dist = math.fabs(self.estimated_obj_dist) # Returns the absolute value of distance
-        #                   pygame.display.get_surface().blit(textSurfstop, textRectstop)
-        #               elif self.estimated_obj_dist > 8: # If object distance is greater than 100 cm. It gets plotted at origin. 8 is the radius of the big circle
-        #                   self.estimated_obj_dist = 0
-        #               x = self.sx/2 + (self.sx/2.5)* math.cos(math.radians(self.angle-180))
-        #               #dy = self.sy/2 + self.sx/2 * math.sin(math.radians(self.angle)) # Starts from top side
-        #               y = self.sy/2 + (self.sy/2.5) * math.sin(math.radians(self.angle))
-        #              # anti aliasing line: To make line smooth
-        #               pygame.draw.aaline(screen, self.brightred, (self.sx/2, self.sy/2), (x, y),5) # Takes about 10 seconds to sweep 180 degrees. Used to be dx and dy
-          #
-        #               rx = int(self.sx/2 - 50 * self.estimated_obj_dist * math.cos(math.radians(self.angle))) # Starts plotting right
-        #               ry = int(self.sy/2 + 50 * self.estimated_obj_dist * math.sin(math.radians(self.angle))) # Starts plotting top
-        #               Rrx2[i/8] = rx
-        #               Rry2[i/8] = ry
-        #               pygame.display.update()
-        #               pygame.time.wait(30) # Sleeps the gui for 30 milliseonds to share CPU. Share with ROS
-        #              # Could also use pygame.time.delay() instead of time.wait
-        #               screen.fill((0, 20, 0, 0))
-          #
-        #         for event in pygame.event.get():
-        #             if event.type == pygame.QUIT:
-        #                 pygame.quit()
-        #                 exit()
-            #     else:
-            #    #print("Landed in el statement")
-            #        time.sleep(0.001) # Used to sleep gui for a bit
 
-          #if self.scan_once_to_herc == 2: # Going back to stop scanning
-            #self.scan_once_to_herc = 1
+
     def pub_stop_button_state(self):
         self.cmd_stop.publish(self.stop)
 
